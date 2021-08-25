7ea7f7464f23b9ec4f22e00990dc0127d9c2de42
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-08-25 16:39:45 -0300

    Requisito 5 - Adiciona um formulario no corpo da pagina

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 function buttonLogin() {
   const loginButton = document.getElementById('login');
   loginButton.addEventListener('click', () => {
-      const userEmail = document.getElementById('user-email').value;
-      const userPassword = document.getElementById('password').value;
-      const trybeEmail = 'tryber@teste.com';
-      const trybePassword = '123456';
-  
-      if(userEmail === trybeEmail && userPassword === trybePassword) {
-        alert('Olá, Tryber!');
-      }else {
-        alert('Email ou senha inválidos.');
-      }
-    });
+    const userEmail = document.getElementById('user-email').value;
+    const userPassword = document.getElementById('password').value;
+    const trybeEmail = 'tryber@teste.com';
+    const trybePassword = '123456';
+
+    if (userEmail === trybeEmail && userPassword === trybePassword) {
+      alert('Olá, Tryber!');
+    } else {
+      alert('Email ou senha inválidos.');
+    }
+  });
 }
 
 buttonLogin();
-
